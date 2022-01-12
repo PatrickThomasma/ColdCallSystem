cf943a7fb94e5c42bfe4c56c0c19a85e6c78add0
Patrick Thomasma <PatThomasma@bitbucket.org>
2022-01-12 15:56:32 -0800

    Making initial class file

--- GUI/backend/objects.py
+++ GUI/backend/objects.py
@@ -1,3 +1,32 @@
 """
 This file will initialize the student and Queue from an input file that has a list of Students and their ID numbers
 """
+
+class Student:
+    def __init__(self,first,last,ID,email,phoneticSpelling,revealCode,LF)
+        self.first = first
+        self.last = last
+        self.ID = ID
+        self.email = email
+        self.phonetic = phoneticSpelling
+        self.reveal = revealCode
+        self.LF = LF
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+        self.length = 0
+
+    def enqueue(self):
+        #This will enqueue our students into the queue
+        pass
+    def dequeue(self):
+        #This will remove a student from the queue
+        pass
+    def isEmpty(self):
+        #This will check if our Queue has any students
+        pass
+    def print(self):
+        #This will be our printing method for our queue/debugging
+        pass
