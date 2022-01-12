adcda75854fc704a478f48c609a970672426a202
Patrick Thomasma <PatThomasma@bitbucket.org>
2022-01-12 15:36:14 -0800

    Tinkering with GUI

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -4,5 +4,15 @@ This will be the main GUI for students
 Author:
 Last Modified:
 
-
+If on Linux Ubuntu use sudo apt-get install python3-tk for tkinter Module
 """
+from tkinter import *
+
+
+root = Tk()
+
+MyLabel = Label (root, text="Hello World!")
+MyLabel.pack()
+
+root.mainloop()
+
